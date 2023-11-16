e1f84a3cc80173f8cf1c02e0d8f6fddcb000196c
SvetlanaNikonova <svetlananikonova@coherentsolutions.com>
2023-11-16 16:59:28 +0200

    corrected paths, 'for' changed into 'foreach', 'try catch' added for parsing

--- src/main/java/files/unzipping/XmlZipTest.java
+++ src/main/java/files/unzipping/XmlZipTest.java
@@ -1,7 +1,6 @@
 package files.unzipping;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -13,6 +12,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -20,9 +21,13 @@ public class XmlZipTest {
 
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
 
-        String zipFilePath = "C:\\Users\\SvetlanaNikonova\\Downloads\\SNC_AOD_01102023_061442.xml.zip";
-        String destDir = "C:\\Users\\SvetlanaNikonova\\Downloads\\output";
-        String filePath = "C:\\Users\\SvetlanaNikonova\\Downloads\\output\\SNC_AOD_01102023_061442.xml";
+        String zipFilePath = ("C:" + File.separator + "Users" + File.separator + "SvetlanaNikonova" +
+                File.separator + "Downloads" + File.separator + "SNC_AOD_01102023_061442.xml.zip");
+        String destDir = ("C:" + File.separator + "Users" + File.separator + "SvetlanaNikonova" +
+                File.separator + "Downloads" + File.separator + "output");
+        String filePath = ("C:" + File.separator + "Users" + File.separator + "SvetlanaNikonova" +
+                File.separator + "Downloads" + File.separator + "output" + File.separator + "SNC_AOD_01102023_061442.xml");
+
 
         unzip(zipFilePath, destDir);
 
@@ -35,51 +40,68 @@ public class XmlZipTest {
         doc.getDocumentElement().normalize();
         System.out.println(doc.getDocumentElement().getNodeName());
 
-        NodeList nList = doc.getElementsByTagName("PaidAmount");
-        int tLength = nList.getLength();
-        System.out.println(tLength);
 
-        double sum = 0.00;
-        for (int i = 0; i < tLength; i++) {
-            Node nNode = nList.item(i);
-
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) nNode;
-                sum += Double.parseDouble(eElement.getTextContent());
-            }
-        }
-        System.out.println("Total sum of all paidAmounts: " + sum);
-    }
-
-        private static void unzip(String zipFilePath, String destDir) {
-            File dir = new File(destDir);
-            if (!dir.exists()) dir.mkdirs();
-            FileInputStream fis;
-            byte[] buffer = new byte[1024];
+        NodeList nodeList = doc.getElementsByTagName("PaidAmount");
+        iterable(nodeList).forEach(node -> {
             try {
-                fis = new FileInputStream(zipFilePath);
-                ZipInputStream zis = new ZipInputStream(fis);
-                ZipEntry ze = zis.getNextEntry();
-                while (ze != null) {
-                    String fileName = ze.getName();
-                    File newFile = new File(destDir + File.separator + fileName);
-                    System.out.println("Unzipping to " + newFile.getAbsolutePath());
-
-                    new File(newFile.getParent()).mkdirs();
-                    FileOutputStream fos = new FileOutputStream(newFile);
-                    int len;
-                    while ((len = zis.read(buffer)) > 0) {
-                        fos.write(buffer, 0, len);
-                    }
-                    fos.close();
-                    zis.closeEntry();
-                    ze = zis.getNextEntry();
-                }
-                zis.closeEntry();
-                zis.close();
-                fis.close();
-            } catch (IOException e) {
+                double sum = Double.parseDouble(node.getTextContent());
+                System.out.println("PaidAmount sum: " + sum);
+            } catch (NumberFormatException e) {
                 e.printStackTrace();
             }
+        });
+    }
+
+
+    public static Iterable<Node> iterable(final NodeList nodeList) {
+        return () -> new Iterator<Node>() {
+
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < nodeList.getLength();
+            }
+
+            @Override
+            public Node next() {
+                if (!hasNext())
+                    throw new NoSuchElementException();
+                return nodeList.item(index++);
+            }
+        };
+    }
+
+
+    private static void unzip(String zipFilePath, String destDir) {
+        File dir = new File(destDir);
+        if (!dir.exists()) dir.mkdirs();
+        FileInputStream fis;
+        byte[] buffer = new byte[1024];
+        try {
+            fis = new FileInputStream(zipFilePath);
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry ze = zis.getNextEntry();
+            while (ze != null) {
+                String fileName = ze.getName();
+                File newFile = new File(destDir + File.separator + fileName);
+                System.out.println("Unzipping to " + newFile.getAbsolutePath());
+
+                new File(newFile.getParent()).mkdirs();
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+                zis.closeEntry();
+                ze = zis.getNextEntry();
+            }
+            zis.closeEntry();
+            zis.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+}
